fb842a11f49b116c340528714da4bf3306a5670f
Thais Regina Paes <thaisreginapaesdossantos@gmail.com>
2023-10-19 10:52:30 -0300

    Feat: exemplo arrays

---
+++ BootcampExemplos/ExemploArrays.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampExemplos.obj
+{
+    public class ExemploArrays
+    {
+        int[] arrayInteiros = new int[3];
+
+        public void PercorrerArray()
+        {
+            arrayInteiros[0] = 72;
+            arrayInteiros[1] = 64;
+            arrayInteiros[2] = 50;
+
+            for (int cont = 0; cont <= arrayInteiros.Length; cont++)
+            {
+                Console.WriteLine($"Posição Nº {cont} - {arrayInteiros[cont]}");
+            }
+        }
+
+        int contadorForeach = 0;
+        public void ForeachArray()
+        {
+            foreach(int valor in arrayInteiros)
+            {
+                Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+                contadorForeach++;
+            }
+        }
+
+        public void AjustandoArray()
+        {
+            Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+        }
+    }
+}
