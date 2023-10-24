963eef8f3361fed10369616fd38a71b34ba6b5ba
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-10-24 12:56:01 -0300

    Feat: implementando a classe curso

---
+++ BootcampDotNet.Common/Models/Curso.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bootcamp_dotnet_qa.Models;
+
+namespace BootcampDotNet.Common.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadedeAlunos()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+
+        }
+
+        public void RemoverAluno(Pessoa aluno)
+        {
+            Alunos.Remove(aluno);
+        }
+
+        public void ListarAlunos()
+        {
+            Console.WriteLine($"Alunos do curso de: {Nome}");
+            foreach (Pessoa aluno in Alunos)
+            {
+                Console.WriteLine(aluno.Fullname);
+            }
+        }
+
+    }
+}
