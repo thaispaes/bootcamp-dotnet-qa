da6fb8427725f43ac98b4cfbbfd72295b35a1032
Thais Paes <thaisreginapaesdossantos@gmail.com>
2023-10-23 21:37:30 -0300

    Feat: melhorando nosse get e set

--- BootcampDotNet.Common/Models/Pessoa.cs
+++ BootcampDotNet.Common/Models/Pessoa.cs
@@ -8,7 +8,24 @@ namespace bootcamp_dotnet_qa.Models
     public class Pessoa
     {
         private string _name;
-        public string Name { get; set; }
+        public string Name 
+        { 
+            get
+            {
+                return _name.ToUpper();
+            } 
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _name = value;
+            }
+        
+        }
         public int Age { get; set; }
 
         public void Apresentar() 
