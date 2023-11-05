d47854d806ebe70472026d0dd3e4ef13001e88c5
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-11-05 16:42:17 -0300

    Feat: desafio 1 finalizado

--- BootcampDotNet/Program.cs
+++ BootcampDotNet/Program.cs
@@ -1,56 +1,15 @@
-﻿using System.Runtime.InteropServices.Marshalling;
-using BootcampDotNet.Common.Models.DesafioFundamentos;
+﻿using System;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+namespace SimulacaoFalhaTeste {
+  class Program {
+    static void Main(string[] args) {
+      
+      string nomeTeste, descricaoDoErro;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+      nomeTeste = Console.ReadLine();
+      descricaoDoErro = Console.ReadLine();
 
-Console.WriteLine("Agora digite o preço por hora estacionada:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
-
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
-
-string opcao = string.Empty;
-bool exibirMenu = true;
-
-// Realiza o loop do menu
-while (exibirMenu)
-{
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
-
-    switch (Console.ReadLine())
-    {
-        case "1":
-            es.AdicionarVeiculo();
-            break;
-
-        case "2":
-            es.RemoverVeiculo();
-            break;
-
-        case "3":
-            es.ListarVeiculos();
-            break;
-
-        case "4":
-            exibirMenu = false;
-            break;
-
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
+      Console.WriteLine($"O teste falhou. Descricao: {descricaoDoErro}");
     }
-
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+  }
 }
-
-Console.WriteLine("O programa se encerrou");
